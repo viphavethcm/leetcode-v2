2aa2f19e8011b46ba35db034bc6504472ee5b5df
viphavethcm <loivang.nhatduy@gmail.com>
2025-02-09 00:08:54 +0700

    recursion easy problem

---
+++ src/recursion/EasyProblem.java
@@ -0,0 +1,22 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class EasyProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        String tmp = "a";
+        System.out.println(findNext(tmp, k));
+    }
+
+    public static char findNext(String rs, int k) {
+        if(rs.length() >= k) {
+            return rs.charAt(k-1);
+        }
+        for(char ch: rs.toCharArray()){
+            rs += Character.toString(ch + 1);
+        }
+        return findNext(rs, k);
+    }
+}
