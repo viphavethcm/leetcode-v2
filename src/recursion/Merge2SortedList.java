b14fd89deda6ef871cf65579eac00b57e3b96a9b
duydoan <duy.doan@tyme.com>
2025-02-10 00:00:51 +0700

    merged 2 sorted list

--- src/recursion/Merge2SortedList.java
+++ src/recursion/Merge2SortedList.java
@@ -1,7 +1,5 @@
 package recursion;
 
-import java.util.List;
-
 public class Merge2SortedList {
 
 //      Definition for singly-linked list.
@@ -15,11 +13,22 @@ public class Merge2SortedList {
 
 
     public static void main(String[] args) {
-        ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, null)));
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4, null)));
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4, null)));
 
+        ListNode rs = merge(l1, l2);
+        System.out.println(rs);
     }
 
-    public static ListNode append() {
-        return null;
+    public static ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = merge(l1.next, l2); // Merge the rest of l1 with l2
+            return l1;  // l1's head is the smallest, so we return l1
+        } else {
+            l2.next = merge(l1, l2.next); // Merge l1 with the rest of l2
+            return l2;  // l2's head is the smallest, so we return l2
+        }
     }
 }
