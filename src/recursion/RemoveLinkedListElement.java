0f252258a116a8b2b2b31e073d0a8e34a349eb1f
duydoan <duy.doan@tyme.com>
2025-02-15 10:45:17 +0700

    remove linked list element

---
+++ src/recursion/RemoveLinkedListElement.java
@@ -0,0 +1,35 @@
+package recursion;
+
+public class RemoveLinkedListElement {
+
+    //      Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4, null)));
+        int val = 2;
+        ListNode rs = removeElements(l1, val);
+        System.out.println(rs);
+    }
+
+    public static ListNode removeElements(ListNode head, int val) {
+        ListNode rep = new ListNode(0, head);
+        resolve(head, rep, val);
+        return rep.next;
+    }
+
+    public static void resolve(ListNode current, ListNode prev, int val) {
+        if (current == null) return;
+        if (current.val == val) {
+            prev.next = removeElements(current.next, val);
+        }
+        current.next = removeElements(current.next, val);
+    }
+}
