69502037cf49ac13cfdd6679551444f1d2fc100a
duydoan <duy.doan@tyme.com>
2025-02-25 13:48:19 +0700

    power of three

---
+++ src/recursion/easy/PowerOfThree.java
@@ -0,0 +1,24 @@
+package recursion.easy;
+
+public class PowerOfThree {
+
+    public static void main(String[] args) {
+        int n = 27;
+        System.out.println(isPowerOfTwo(n));
+    }
+
+    public static boolean isPowerOfTwo(int n) {
+        if(n == 1)
+            return true;
+        else if(n < 3)
+            return false;
+        return resolve(n, 3) == n;
+    }
+
+    public static long resolve(int n, long tmp) {
+        if (tmp >= n)
+            return tmp;
+        tmp  = resolve(n, tmp * 3);
+        return tmp;
+    }
+}
